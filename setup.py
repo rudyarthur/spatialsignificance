0a3d1ea80bb44dd25ba807dd8342df02471c91d7
rudyarthur <rudy.d.arthur@gmail.com>
2025-02-05 05:41:47 +0000

    pip stuff

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
-from setuptools import Extension, setup
+from setuptools import Extension, find_packages, setup
 from Cython.Build import cythonize
+import numpy
 
 extensions = [
-    Extension("spatialsignificance", ["src/spatialsignificance.pyx"], define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")], language="c++"),
+    Extension("spatialsignificance", ["src/spatialsignificance.pyx"], define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],include_dirs=[numpy.get_include()]),
 ]
 setup(
+   packages=find_packages(where="src"), 
+   package_dir={"": "src"},
+   package_data = {"": ["*.pyx"] },
    ext_modules = cythonize(extensions, annotate=True, 
    compiler_directives={'language_level' : "3", "cdivision":True, "boundscheck":False, "wraparound":False, "nonecheck":False}
-   ) 
+   ),
+   python_requires=">=3.9",
+   install_requires=["numpy>=2.0.0", "Cython>=3.0.10" ],
+   setup_requires=["numpy>=2.0.0", "Cython>=3.0.10" ],
+   include_package_data=True,  
+   zip_safe=False,
 )
--- upload.sh
+++ upload.sh
@@ -1,6 +1,8 @@
-##add something so I don't have to change the version number by hand!
+version=0.0.2
+sed -e s/VERSION_NUMBER/$version/g pyproject.toml.template > pyproject.toml
+sed -e s/VERSION_NUMBER/$version/g setup.cfg.template > setup.cfg
 rm dist/*
 python3 setup.py sdist bdist_wheel
-auditwheel repair dist/valeriepieris-0.1.4-cp38-cp38-linux_x86_64.whl --plat manylinux_2_17_x86_64
-mv wheelhouse/* dist; rm dist/valeriepieris-0.1.4-cp38-cp38-linux_x86_64.whl
-python3 -m twine upload --repository pypi dist/*
+auditwheel repair dist/spatialsignificance-"$version"-cp310-cp310-linux_x86_64.whl --plat manylinux_2_17_x86_64
+mv wheelhouse/* dist; rm dist/spatialsignificance-"$version"-cp310-cp310-linux_x86_64.whl
+python3 -m twine upload --repository pypi dist/* --verbose
